6bfd97be8d8dbc99ff94c784c8a1d071658e7366
murilo.schiavotto@gmail.com <murilo.schiavotto@gmail.com>
2025-09-13 19:34:51 -0300

    Adicionando ícones ao app

--- SecuritySystemApp/Components/BottomTabsView.xaml.cs
+++ SecuritySystemApp/Components/BottomTabsView.xaml.cs
@@ -4,12 +4,35 @@ using SecuritySystemApp.Interfaces;
 
 namespace SecuritySystemApp.Components;
 
+public enum BottomTabPage
+{
+    NotifyPage,
+    HomePage,
+    ConfigPage
+}
+
 public partial class BottomTabsView : ContentView
 {
     private readonly INavigationService _navigationService;
+
+    public static readonly BindableProperty CurrentPageProperty =
+        BindableProperty.Create(
+            nameof(CurrentPage),
+            typeof(BottomTabPage),
+            typeof(BottomTabsView),
+            BottomTabPage.HomePage,
+            propertyChanged: OnCurrentPageChanged);
+
+    public BottomTabPage CurrentPage
+    {
+        get => (BottomTabPage)GetValue(CurrentPageProperty);
+        set => SetValue(CurrentPageProperty, value);
+    }
+
     public BottomTabsView()
     {
         InitializeComponent();
+        UpdateIcons();
 
         // Definição dos Serviços
         _navigationService = new NavigationService();
@@ -17,7 +40,6 @@ public partial class BottomTabsView : ContentView
 
     private async void OnNotifyClicked(object sender, EventArgs e)
     {
-        // Configurar rota para alguma página
         await _navigationService.NavegarParaAsync(nameof(NotifyPage));
     }
 
@@ -29,5 +51,33 @@ public partial class BottomTabsView : ContentView
     private async void OnConfigClicked(object sender, EventArgs e)
     {
         await _navigationService.NavegarParaAsync(nameof(ConfigPage));
+    }
+
+    private static void OnCurrentPageChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (BottomTabsView)bindable;
+        control.UpdateIcons();
+    }
+
+    private void UpdateIcons()
+    {
+        if (CurrentPage == BottomTabPage.NotifyPage)
+        {
+            if (Application.Current?.RequestedTheme == AppTheme.Dark)
+                NotifyButton.ImageSource = "notify_white_full.png";
+            else
+                NotifyButton.ImageSource = "notify_black_full.png";
+        }
+        if (CurrentPage == BottomTabPage.HomePage)
+        {
+            HomeButton.ImageSource = "homeicon.png";
+        }
+        if (CurrentPage == BottomTabPage.ConfigPage)
+        {
+            if (Application.Current?.RequestedTheme == AppTheme.Dark)
+                ConfigButton.ImageSource = "config_white_full.png";
+            else
+                ConfigButton.ImageSource = "config_black_full.png";
+        }
     }
 }
