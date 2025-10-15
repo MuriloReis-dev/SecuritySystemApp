39bfa9b906dc9f5fb51d3a4e859c07ebd091c7be
murilo.schiavotto@gmail.com <murilo.schiavotto@gmail.com>
2025-10-14 22:09:24 -0300

    Alterando visibilidade dos elementos na AlarmePage para melhor UX

--- SecuritySystemApp/Models/NotifyDTO.cs
+++ SecuritySystemApp/Models/NotifyDTO.cs
@@ -3,7 +3,7 @@ namespace SecuritySystemApp.Models;
 // Model que retorna a resposta ao logar, com dados do usuário e um token de autenticação
 public class NotifyDTO
 {
-    public int Id { get; set; }
+    public int IdNotificacao { get; set; }
     public string? Mensagem { get; set; }
     public DateTime DataHora { get; set; }
 }
